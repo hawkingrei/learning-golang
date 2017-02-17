52fea292bfd752ca765c4c164242d9b358e2b04a
真人 <weizhen.wang@duitang.com>
2017-02-17 23:58:01 +0800

    update

--- pratice/point/test.go
+++ pratice/point/test.go
@@ -8,12 +8,17 @@ func main(){
 	type entry struct {
 		key   interface{}
 		value interface{}
+		mp    map[string]interface{}
 	}
 	
 	
 	var mylist *list.List
+	var mymap map[string]*entry
+	mymap = make(map[string]*entry)	
 	mylist = list.New()
-	ent := &entry{"wwz","abc"}
+	ent := &entry{"wwz","abc",make(map[string]interface{})}
+	mymap["wwz"] = &entry{"wwz","abc",make(map[string]interface{})}
+	(*mymap["wwz"]).mp["wwz"] = list.New()
 	mylist.PushFront(&ent)
 	
 	
